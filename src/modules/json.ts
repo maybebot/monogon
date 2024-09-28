06dd35f6dc662d9f01c82a228f8d79216871207f
Ian Bobinac <ian@ianb.dev>
2024-09-28 15:03:04 +0200

    feat(json): improve highlight, catch errors on invalid json

--- src/modules/json.ts
+++ src/modules/json.ts
@@ -1,18 +1,23 @@
 const definitions = [
   { name: 'any', regex: /.*/g, css: { color: '#ce916f' } },
-  { name: 'string', regex: /"(?:[^"\\]*(?:\\.[^"\\]*)*)"/g, css: { color: '#ce916f' } },
-  { name: 'key', regex: /"([^"\\]*(\\.[^"\\]*)*)"\s*:/g, css: { color: '#9ad9fb' } },
-  // { name: 'array', regex: /"([^"\\]*(\\.[^"\\]*)*)"/g, css: { color: '#f00' } },
-
   { name: 'number', regex: /-?\d+(\.\d+)?([eE][+-]?\d+)?/g, css: { color: '#b5cea8' } },
   { name: 'boolean', regex: /\b(true|false)\b/g, css: { color: '#569cd6' } },
   { name: 'null', regex: /\bnull\b/g, css: { color: '#569cd6' } },
   { name: 'object-brackets', regex: /[{}]/g, css: { color: '#ffd708' } },
   { name: 'array-brackets', regex: /[[\]]/g, css: { color: '#da70b9' } },
+  { name: 'string', regex: /"(?:[^"\\]*(?:\\.[^"\\]*)*)"/g, css: { color: '#ce916f' } },
+  { name: 'key', regex: /"([^"\\]*(\\.[^"\\]*)*)"\s*:/g, css: { color: '#9ad9fb' } },
 ];
 
 const format = (json: string) => {
-  return JSON.stringify(JSON.parse(json), null, 2);
+  try {
+    return JSON.stringify(JSON.parse(json), null, 2);
+  } catch {
+    if (import.meta.env.DEV) {
+      console.warn('%c [monogon] ', 'color: #bada55', 'Invalid Json, unable to format, skipping');
+    }
+    return json;
+  }
 };
 
 export const json = {
