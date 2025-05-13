cf9329d09440f7276376af5e28d3a4a2942f2f6a
Ian Bobinac <ian@ianb.dev>
2025-05-13 18:25:10 +0200

    fix: improve jsx support for webpack builds without jsx loader

--- src/frameworks/preact/MonogonCode.tsx
+++ src/frameworks/preact/MonogonCode.tsx
@@ -1,6 +1,6 @@
 import '../../monogon-code.js';
 
-const MonogonCode = ({ content, lang }: { content: string; lang: string }) => {
+const MonogonCode = ({ content, lang }: { content: string; lang: string; onInput?: () => void }) => {
   // @ts-expect-error JSX not existing
   return <monogon-code content={content} lang={lang}></monogon-code>;
 };
--- src/frameworks/solid/MonogonCode.tsx
+++ src/frameworks/solid/MonogonCode.tsx
@@ -1,6 +1,6 @@
 import '../../monogon-code.js';
 
-export default function ({ content, lang }: { content: string; lang: string }) {
+export default function ({ content, lang }: { content: string; lang: string; onInput?: () => void }) {
   // @ts-expect-error JSX not existing
   return <monogon-code attr:content={content} attr:lang={lang}></monogon-code>;
 }
