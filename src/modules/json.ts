7c9f298fd460afca3e91a0973bd853feedb56265
Ian Bobinac <ian@ianb.dev>
2026-06-03 09:06:31 +0200

    feat: improve json support

--- src/modules/json.ts
+++ src/modules/json.ts
@@ -1,13 +1,16 @@
-import { monoLog } from '../utils.js';
-import { cssProps } from '../theme.js';
+import { monoLog } from "../utils.js";
+import { cssProps } from "../theme.js";
 
 const definitions = {
   any: { regex: /.*/g, css: cssProps.text },
-  number: { regex: /-?\d+(\.\d+)?([eE][+-]?\d+)?/g, css: cssProps.number },
+  number: {
+    regex: /(?<![\w.])-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?(?![\w.])/g,
+    css: cssProps.number,
+  },
   boolean: { regex: /\b(true|false)\b/g, css: cssProps.boolean },
   null: { regex: /\bnull\b/g, css: cssProps.null },
   objectBrackets: { regex: /[{}]/g, css: cssProps.curlyBrackets },
-  arrayBrackets: { regex: /[[\]]/g, css: cssProps.squareBrackets },
+  arrayBrackets: { regex: /[\[\]]/g, css: cssProps.squareBrackets },
   string: { regex: /"(?:[^"\\]*(?:\\.[^"\\]*)*)"/g, css: cssProps.string },
   key: { regex: /"([^"\\]*(\\.[^"\\]*)*)"\s*:/g, css: cssProps.key },
 };
@@ -16,7 +19,7 @@ const format = (content: string) => {
   try {
     return JSON.stringify(JSON.parse(content), null, 2);
   } catch {
-    monoLog('Invalid JSON, unable to format, skipping');
+    monoLog("Invalid JSON, unable to format, skipping");
     return content;
   }
 };
